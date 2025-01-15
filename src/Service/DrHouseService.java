0f7b01467d451f137399586966ac1b53d38ef01f
AndreiLapadatu <andrei.lapadatu@stud.ubbcluj.ro>
2025-01-15 15:26:00 +0200

    d

--- src/Service/DrHouseService.java
+++ src/Service/DrHouseService.java
@@ -1,8 +1,10 @@
 package Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
 import Model.*;
 
 public class DrHouseService {
@@ -17,6 +19,29 @@ public class DrHouseService {
             }
         }
         return list;
+    }
+
+
+    public void calculator(List<DrHouse> drHouses,String filename) {
+
+        Map<String,Integer> points = new HashMap<>();
+        for(DrHouse drHouse : drHouses) {
+            if(drHouse.getKrankenhaus().equals(drHouse.getKrankenhaus()))
+                points.put(drHouse.getKrankenhaus(), points.getOrDefault(drHouse.getKrankenhaus(), 0) + 1);
+        }
+
+
+        List<Map.Entry<String,Integer>> sortedPoints = new ArrayList<>(points.entrySet());
+        sortedPoints.sort((a,b) -> b.getValue().compareTo(a.getValue()));
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for(Map.Entry<String,Integer> entry : sortedPoints) {
+                writer.write(entry.getKey() + "$" + entry.getValue() );
+                writer.newLine();
+            }
+        }catch (IOException e){
+            System.out.println("Error writing in file"+ e.getMessage());
+        }
     }
 
 }
