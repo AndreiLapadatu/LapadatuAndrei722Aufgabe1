b1eaa9be55ffeee74fff00c6c2105844b80fd7d0
AndreiLapadatu <andrei.lapadatu@stud.ubbcluj.ro>
2025-01-15 15:20:42 +0200

    b

--- src/Service/DrHouseService.java
+++ src/Service/DrHouseService.java
@@ -3,8 +3,20 @@ package Service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import Model.*;
 
 public class DrHouseService {
 
+
+
+    public List<String> filterByFirstLetterUppercase(List<DrHouse> drHouses,char bigletter) {
+        List<String> list = new ArrayList<>();
+        for(DrHouse drHouse : drHouses) {
+            if(drHouse.getPatient().charAt(0) == bigletter) {
+                list.add(drHouse.getPatient());
+            }
+        }
+        return list;
+    }
 
 }
